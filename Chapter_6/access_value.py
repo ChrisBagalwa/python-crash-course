879c3c7ba826401f2bad108c1a844f70e36c1d4e
Chris Bagalwa <60501135+ChrisBagalwa@users.noreply.github.com>
2023-06-20 22:36:27 +0200

    Add files via upload

---
+++ Chapter_6/access_value.py
@@ -0,0 +1,8 @@
+# This program shows how to access a dictionary
+# Author: Chris Bagalwa
+# 20/06/2023
+
+alien_o = {'color':'green', 'points':5}
+
+new_points = alien_o['points']
+print(f"You just earned {new_points} points!")
---
+++ Chapter_6/simple_dictionary.py
@@ -0,0 +1,8 @@
+# This program contains a dictionary that stores information about a particular alien
+# Author: Chris Bagalwa
+# 20/06/2023
+
+alien_o = {'color':'green', 'points':5}
+
+print(alien_o['color'])
+print(alien_o['points'])
